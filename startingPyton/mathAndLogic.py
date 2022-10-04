e5c49b9111c251ad9c48865b8318d7530c422892
SalahSoussou <kaka79702@gmail.com>
2022-10-04 18:59:07 +0100

    ok

---
+++ startingPyton/mathAndLogic.py
@@ -0,0 +1,42 @@
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 / 2)
+
+# === logical operators === #
+
+a = True
+b = False
+
+if a and b :
+    print('All true')
+elif a or b:
+    print('One true')
+
+if not(b):
+    print('b is not true')
+
+# ==== === === === #
+
+num = 114
+
+subNum = 10
+
+if num > 100:
+    print('number is greater than 100!')
+    num = num - subNum
+
+print('New Number: ' +str(num))
+
+
+
+
+
+
+
+
+
+
+
+
+
