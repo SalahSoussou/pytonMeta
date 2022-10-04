2a9bcbfa23c8113691ead2be74a766b6e9ba3ad6
SalahSoussou <kaka79702@gmail.com>
2022-10-04 14:48:59 +0100

    ok

---
+++ startingPyton/index.py
@@ -0,0 +1,12 @@
+
+print('hello World'); print('salah')
+
+x = 1 + 3 \
++ 4
+print(x)
+
+user = 'Salah'
+if user == 'Salah':
+    print(user)
+
+    
---
+++ startingPyton/variables.py
@@ -0,0 +1,28 @@
+x = 10
+x = 20
+print(x) #20
+
+a = b = 10
+print(a) #10
+print(b) #10
+
+a = 22
+print(a) #22
+print(b) #10
+
+e,f,j = 1,2,3
+
+print(f) #2
+
+del e  #delet the varible
+# print(e) # e is not defined
+
+print(type(10)) #<class 'int'>
+
+print(type(1.5))  #<class 'float'>
+
+#===================== input()
+
+email = input('inter your email: ')
+print(email)
+print('soussou','salah' ,sep=', ') #soussou , salas
