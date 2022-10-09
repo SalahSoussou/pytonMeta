4e8ee4aa5295da66e83ef8d8a789d3f8b1cfd6b8
SalahSoussou <kaka79702@gmail.com>
2022-10-09 19:10:57 +0100

    ok

--- startingPyton/opp.py
+++ startingPyton/opp.py
@@ -1,3 +1,6 @@
+from re import X
+
+
 def sum(n):
     if n==1:
         return 0
@@ -5,3 +8,34 @@ def sum(n):
 
 print(sum(5))
 
+# Class ====  #
+
+class MyClass:
+    print('hello from class')
+    a = 'var in class'
+    def func(self):
+        print('class func')
+
+my_class = MyClass()
+#print(my_class.a)
+#print(my_class.func())
+
+class House:
+    rooms = 5
+    bath = 2
+    def evalu(self):
+        print(self.rooms)
+        pass
+
+
+class Ricipe():
+    def __init__(self,x,y) -> None:
+        self.x = x
+        self.y = y 
+    def my_func(self):
+        print('cordone: x => '+ self.x + '\n         y => '+self.y)
+
+
+my_class = Ricipe('233','786')
+
+my_class.my_func()
