1f7ca3dfc6702e55f8745e73a21921ebfa061b96
SalahSoussou <kaka79702@gmail.com>
2022-10-14 18:02:13 +0100

    ok

--- startingPyton/moduls/reload.py
+++ startingPyton/moduls/reload.py
@@ -9,11 +9,3 @@ importlib.reload(sampel)
 "https://github.com/Meta-Front-End-Developer-PC/m3l2_forking_lab/pull/1783"
 
 
-# testing typs 
-#  
-
-def add(a,b):
-    return a+b
-
-def sub(a,b):
-    return a-b
