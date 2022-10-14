70aaee951384a2319ef51ca033458d5bcc00b3bd
SalahSoussou <kaka79702@gmail.com>
2022-10-14 12:19:59 +0100

    ok

--- startingPyton/moduls/reload.py
+++ startingPyton/moduls/reload.py
@@ -1,9 +1,10 @@
 import importlib
-import sampl
+import oo as sampel
 
-importlib.reload(sampl)
-importlib.reload(sampl)
-importlib.reload(sampl)
-importlib.reload(sampl)
+importlib.reload(sampel)
+importlib.reload(sampel)
+importlib.reload(sampel)
+importlib.reload(sampel)
 "SalahSoussou:my-branch"
 "https://github.com/Meta-Front-End-Developer-PC/m3l2_forking_lab/pull/1783"
+
